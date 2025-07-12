0390afb5f70a8798e85440ababc02b010fc66f19
ANKIT RAJ <ankitraj311@users.noreply.github.com>
2025-07-12 19:31:14 +0530

    Create 1_Plus_Minus.cpp

---
+++ 1_Plus_Minus.cpp
@@ -0,0 +1,16 @@
+void plusMinus(vector<int> arr) {
+    
+    int countP=0,countN=0,countZ=0, array_size=arr.size();
+    for(int i=0;i<array_size;i++)
+    {
+        if(arr[i]>0)
+            countP++;
+        if(arr[i]<0)
+            countN++;
+        if(arr[i]==0)
+            countZ++;
+    }
+    cout<<fixed<<setprecision(6)<<(double)countP/array_size<<endl;
+    cout<<fixed<<setprecision(6)<<(double)countN/array_size<<endl;
+    cout<<fixed<<setprecision(6)<<(double)countZ/array_size<<endl;
+}
