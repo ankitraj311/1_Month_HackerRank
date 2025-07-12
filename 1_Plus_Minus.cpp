55d79acaaaeae1c6564006b291f15a0e4fb72973
ANKIT RAJ <ankitraj311@users.noreply.github.com>
2025-07-12 19:43:01 +0530

    Update 1_Plus_Minus.cpp

--- 1_Plus_Minus.cpp
+++ 1_Plus_Minus.cpp
@@ -1,3 +1,5 @@
+
+//need to add header file for cout //#include <iomanip>
 void plusMinus(vector<int> arr) {
     
     int countP=0,countN=0,countZ=0, array_size=arr.size();
@@ -13,4 +15,8 @@ void plusMinus(vector<int> arr) {
     cout<<fixed<<setprecision(6)<<(double)countP/array_size<<endl;
     cout<<fixed<<setprecision(6)<<(double)countN/array_size<<endl;
     cout<<fixed<<setprecision(6)<<(double)countZ/array_size<<endl;
+
+    //printf("%0.06f\n",(float)countP/array_size);
+    //printf("%0.06f\n",(float)countN/array_size);
+    //printf("%0.06f\n",(float)countZ/array_size);
 }
