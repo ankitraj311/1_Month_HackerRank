0a7010f2224f18875e430afb3b33c3381f2a6432
ANKIT RAJ <ankitraj311@users.noreply.github.com>
2025-08-13 10:41:19 +0530

    Update 23_fail_Sherlock_Array.cpp

--- 23_fail_Sherlock_Array.cpp
+++ 23_fail_Sherlock_Array.cpp
@@ -4,13 +4,13 @@ string balancedSums(vector<int> arr) {
     for(int i=0;i<n;i++)
     {
     long long  LSum=0,RSum=0;
-     for(int j=0;j<i;++j)
+     for(int j=0;j<i;j++)
        {
               LSum=LSum+arr[j];
              // cout<<"LSum is "<<LSum<<" When arr[j] value is "<<arr[j]<<" J is "<<j<<endl;
           }
     
-     for(int k=i+1;k<n;++k)
+     for(int k=i+1;k<n;k++)
      {
          RSum=RSum+arr[k];
          // cout<<"RSum is "<<RSum<<" When arr[k] value is "<<arr[k]<<" k is "<<k<<endl;
